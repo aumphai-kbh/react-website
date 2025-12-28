a690484243f7a69ef0c8f01e8486fc6035f9af10
aumphai-kbh <aumphai00@gmail.com>
2025-12-28 22:37:08 +0700

    Change letter

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -9,11 +9,7 @@ const AboutPage = () => {
         We are passionate about building simple, useful, and user-friendly
         digital solutions. Our goal is to learn, grow, and create applications
         that solve real-world problems while delivering a smooth user
-        experience. With a strong interest in modern web technologies, we focus
-        on continuous improvement, clean design, and practical functionality.
-        This website is a space to share our work, ideas, and projects as we
-        continue developing our skills and exploring new opportunities in
-        technology.
+        experience.
       </p>
     </div>
   );
